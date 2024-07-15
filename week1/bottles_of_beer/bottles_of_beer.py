bf3ec2eb2af249741978b56cc11a4b6c61094c65
DarkZelda11 <161272997+DarkZelda11@users.noreply.github.com>
2024-07-15 12:07:36 +0800

    Update bottles_of_beer.py

--- week1/bottles_of_beer/bottles_of_beer.py
+++ week1/bottles_of_beer/bottles_of_beer.py
@@ -1,1 +1,9 @@
+bottles = 99
+
+while bottles > 0:
+    print(str(bottles)+" bottles of beer on the wall")
+    print(str(bottles)+" bottles of beer")
+    print("take one down, pass it around")
+    print(str(bottles - 1)+" bottles of beer on the wall")
+    bottles -= 1
 
